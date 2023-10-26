ee840aadac75254e6832d466f0b629c17ced65ff
FJesus <9150@cifpceuta.es>
2023-10-26 15:51:57 +0200

    Initial Commit

---
+++ app/src/main/java/com/cifpceuta/layout/ActivityLinea.java
@@ -0,0 +1,14 @@
+package com.cifpceuta.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityLinea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linea);
+    }
+}
